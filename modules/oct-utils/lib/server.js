a8c711870e686c9438973413f330f222090b4644
Denis Zatsepin <denis@zatsepin.spb.ru>
2014-04-17 19:34:14 +0400

    Change cookie parser package and add favicon

--- modules/oct-utils/lib/server.js
+++ modules/oct-utils/lib/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var favicon = require('static-favicon');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var Cookies = require('cookies');
+var cookieParser = require('cookie-parser');
 var session = require('express-session');
 var serveStatic = require('serve-static');
 var errorHandler = require('errorhandler');
@@ -12,40 +12,40 @@ var RedisStore = require('connect-redis')(session);
 var app = null;
 
 module.exports.setup = function() {
-  app = express();
+    app = express();
 
-  var sessionOptions = {
-    secret: config.get('server.secret'),
-    key: config.get('server.session.key'),
-    cookie: {
-      maxAge: config.get('server.session.maxAge') * 3600000,
-      secure: config.get('server.session.secure')
-    },
-    store: new RedisStore({client: redis.client})
-  };
-  
-	app.use(favicon(config.root + '/public/favicon.ico'));
-	app.use(bodyParser({ uploadDir: config.root + '/uploads' }));
-	app.use(methodOverride());
-	app.use(Cookies.express(config.get('server.session.key')));
-	app.use(session(sessionOptions));
+    var sessionOptions = {
+        secret: config.get('server.secret'),
+        key: config.get('server.session.key'),
+        cookie: {
+            maxAge: config.get('server.session.maxAge') * 3600000,
+            secure: config.get('server.session.secure')
+        },
+        store: new RedisStore({client: redis.client})
+    };
 
-  module.exports.server = app;
-  return app;
+    app.use(favicon(config.root + '/public/favicon.ico'));
+    app.use(bodyParser({ uploadDir: config.root + '/uploads' }));
+    app.use(methodOverride());
+    app.use(cookieParser());
+    app.use(session(sessionOptions));
+
+    module.exports.server = app;
+    return app;
 };
 
 module.exports.postInstall = function () {
-  app.use(serveStatic(config.root + '/public'));
-  
-  app.use( function(req, res, next) {
-    res.send(404, "Sorry, but page with url " + req.url + " doesn't exist.");
-  });
+    app.use(serveStatic(config.root + '/public'));
 
-	var env = process.env.NODE_ENV || 'development';
-  if ('development' === env) {
-    app.use(errorHandler({
-      showStack: true,
-      dumpExceptions: true
-    }));
-  }
+    app.use( function(req, res, next) {
+        res.send(404, "Sorry, but page with url " + req.url + " doesn't exist.");
+    });
+
+    var env = process.env.NODE_ENV || 'development';
+    if ('development' === env) {
+        app.use(errorHandler({
+            showStack: true,
+            dumpExceptions: true
+        }));
+    }
 };
